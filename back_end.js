1059c464f982ef7ff60473e87142cdc07be1b95b
EmanueleVoltolini <55618574+EmanueleVoltolini@users.noreply.github.com>
2020-08-26 23:13:02 +0200

    intersection function

--- back_end.js
+++ back_end.js
@@ -33,6 +33,65 @@ function mirror_source(edge,source){
     return [x_out,y_out]
 }
 
-function intersection(edge,point_a,pont_b){
-
+function intersection(edge,point_a,point_b){
+    var y_int;
+    var x_int;
+    var q_ab;
+    var m_ab;
+    var q_edge;
+    var m_edge;
+    //check the case when the edge is vertical
+    if (edge.x_a == edge.x_b && (point_a[0] != point_b[0] || point_a[1] != point_b[1])){
+        y_int = edge.y_a;
+        x_int = (point_b[0] - point_a[0]) * ((y_int - point_a[1])/(point_b[1] - point_a[1])) + point_a[0];
+    }
+    //check the case when the edge is horizontal
+    else if (edge.y_a == edge.y_b && (point_a[0] != point_b[0] || point_a[1] != point_b[1])){
+        x_int = edge.x_a;
+        y_int = (point_b[1] - point_a[1]) * ((x_int - point_a[0])/(point_b[0] - point_a[0])) + point_a[1];
+    }
+    else if (point_a[0] == point_b[0]){             //case in which the segment AB is vertical
+        if (edge.x_a == edge.x_b){                  //case parallel segments
+            return 0;
+        }
+        else if (edge.y_a == edge.y_b){             //case AB is vertical and edge is horizontal
+            if (point_a[0]<= Math.max(edge.x_a,edge.x_b) && point_a[0]>= Math.min(edge.x_a,edge.x_b)){
+                return 1;
+            }
+            else{return 0}
+        }
+        else{
+            y_int = point_a[1];
+            x_int = (edge.x_b - edge.x_a) * ((y_int - edge.y_a)/(edge.y_b - edge.y_a)) + edge.x_a;
+        }
+    }
+    else if (point_a[1] == point_b[1]){             //case in which the segment AB is horizontal
+        if (edge.y_a == edge.y_b){                  //case vertical segments
+            return 0;
+        }
+        else if (edge.x_a == edge.x_b){             //case AB is horizontal and edge is vertical
+            if (point_a[1]<= Math.max(edge.y_a,edge.y_b) && point_a[1]>= Math.min(edge.y_a,edge.y_b)){
+                return 1;
+            }
+            else{return 0}
+        }
+        else{
+            x_int = point_a[0];
+            y_int = (edge.y_b - edge.y_a) * ((x_int - edge.x_a)/(edge.x_b - edge.x_a)) + edge.y_a;
+        }
+    }
+    else{                                           //other cases
+        m_edge = (edge.y_b - edge.y_a)/(edge.x_b - edge.x_a);
+        q_edge = edge.y_a - m_edge * edge.x_a;
+        m_ab = (point_b[1] - point_a[1])/(point_b[0] - point_a[0]);
+        q_ab = point_b[1] - m_ab * point_b[0];
+        x_int = (q_ab - q_edge)/(m_edge - m_ab);
+        y_int = m_ab * x_int + q_ab;
+    }          
+    if((Math.min(edge.x_a,edge.x_b)<=x_int && Math.max(edge.x_a,edge.x_b)>=x_int)||(Math.min(edge.y_a,edge.y_b)<=y_int && Math.max(edge.y_a,edge.y_b)>=y_int)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
